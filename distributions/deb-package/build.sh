f88091147141ef0c1d75d3bddac58d2f00a71647
msimonin <matthieu.simonin@inria.fr>
2013-12-01 20:34:39 +0100

    fix deb package version number before release.
    M

--- distributions/deb-package/build.sh
+++ distributions/deb-package/build.sh
@@ -20,7 +20,7 @@
 #
 
 SNOOZE_PACKAGE_NAME="snoozeec2"
-SNOOZE_JAR_NAME="uber-snoozeec2-0.0.1-SNAPSHOT.jar"
+SNOOZE_JAR_NAME="uber-snoozeec2-0.0.1.jar"
 
 # Update config files
 cp -R ../../configs/snooze_ec2.cfg $SNOOZE_PACKAGE_NAME/debian/input/configs/
