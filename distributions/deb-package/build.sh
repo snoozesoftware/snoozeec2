4df8a518412dc90fedb16f84530782b82f445448
msimonin <matthieu.simonin@inria.fr>
2014-03-28 09:19:35 +0100

    increment deb version (prepare release) M

--- distributions/deb-package/build.sh
+++ distributions/deb-package/build.sh
@@ -20,7 +20,7 @@
 #
 
 SNOOZE_PACKAGE_NAME="snoozeec2"
-SNOOZE_JAR_NAME="uber-snoozeec2-2.1.2-SNAPSHOT.jar"
+SNOOZE_JAR_NAME="uber-snoozeec2-2.1.2.jar"
 
 # Update config files
 cp -R ../../configs/snooze_ec2.cfg $SNOOZE_PACKAGE_NAME/debian/input/configs/
