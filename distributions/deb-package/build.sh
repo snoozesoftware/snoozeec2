c100d62a1dbc58a464f0c63788606937598ab45d
msimonin <matthieu.simonin@inria.fr>
2014-04-24 17:14:08 +0200

    post release.
    M

--- distributions/deb-package/build.sh
+++ distributions/deb-package/build.sh
@@ -20,7 +20,7 @@
 #
 
 SNOOZE_PACKAGE_NAME="snoozeec2"
-SNOOZE_JAR_NAME="uber-snoozeec2-2.1.4.jar"
+SNOOZE_JAR_NAME="uber-snoozeec2-2.1.5-SNAPSHOT.jar"
 
 # Update config files
 cp -R ../../configs/snooze_ec2.cfg $SNOOZE_PACKAGE_NAME/debian/input/configs/
